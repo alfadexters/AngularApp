2cd79bfbd2a0fb63f788ec4cf7a01b4c7bdf84db
alfadexters <kietsteven3@gmail.com>
2024-02-03 20:49:43 -0500

    se agrega el proyecto

---
+++ src/app/service/api.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private urlApi='http:localhost:8080/history/mysql/all'
+
+  constructor(private http: HttpClient) { }
+
+    public getData():Observable<any>{
+      return this.http.get<any>(this.urlApi);
+    }
+}
